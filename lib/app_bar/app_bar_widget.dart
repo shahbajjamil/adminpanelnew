0018c143890394dcaaa6424ebbdbbd8ec87b463a
shahbajjamil <shahbajjamil2014@gmail.com>
2021-10-02 01:00:42 +0530

    left page bg

--- lib/app_bar/app_bar_widget.dart
+++ lib/app_bar/app_bar_widget.dart
@@ -18,142 +18,168 @@ class _AppBarWidgetState extends State<AppBarWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        color: Constants.purpleLight,
-        child: Row(
-          children: [
-            if (ResponsiveLayout.isComputer(context))
-              Container(
-                margin: const EdgeInsets.all(Constants.kPadding),
-                height: double.infinity,
-                decoration: const BoxDecoration(
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black45,
-                      offset: Offset(0, 0),
-                      spreadRadius: 1,
-                      blurRadius: 10,
-                    ),
-                  ],
-                  shape: BoxShape.circle,
-                ),
-                child: const CircleAvatar(
-                  backgroundColor: Colors.pink,
-                  radius: 30,
-                  child: FlutterLogo(
-                    size: 40,
+      color: Constants.purpleLight,
+      child: Row(
+        children: [
+          if (ResponsiveLayout.isComputer(context))
+            Container(
+              margin: const EdgeInsets.all(Constants.kPadding),
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    offset: Offset(0, 0),
+                    spreadRadius: 1,
+                    blurRadius: 10,
                   ),
-                ),
-              )
-            else
-              IconButton(
-                  onPressed: () {
-                    Scaffold.of(context).openDrawer();
-                  },
-                  iconSize: 30,
-                  color: Colors.white,
-                  icon: const Icon(Icons.menu)),
-            const SizedBox(
-              width: Constants.kPadding,
-            ),
-            Spacer(),
-            if (ResponsiveLayout.isComputer(context))
-              ...List.generate(
-                _buttonNames.length,
-                (index) => TextButton(
-                  onPressed: () {
-                    setState(() {
-                      _currentSelectedButton = index;
-                    });
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.all(Constants.kPadding * 2),
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      children: [
-                        Text(
-                          _buttonNames[index],
-                          style: TextStyle(
-                            color: _currentSelectedButton == index
-                                ? Colors.white
-                                : Colors.white70,
-                          ),
-                        ),
-                        Container(
-                          margin: const EdgeInsets.all(Constants.kPadding / 2),
-                          width: 60,
-                          height: 2,
-                          decoration: BoxDecoration(
-                              gradient: _currentSelectedButton == index
-                                  ? const LinearGradient(
-                                      colors: [
-                                        Constants.redDark,
-                                        Constants.orangeDark,
-                                      ],
-                                    )
-                                  : null),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-              )
-            else
-              Padding(
-                padding: const EdgeInsets.all(Constants.kPadding * 2),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    Text(
-                      _buttonNames[_currentSelectedButton],
-                      style: const TextStyle(color: Colors.white),
-                    ),
-                    Container(
-                      margin: const EdgeInsets.all(Constants.kPadding / 2),
-                      width: 60,
-                      height: 2,
-                      decoration: const BoxDecoration(
-                          gradient: LinearGradient(
-                        colors: [
-                          Constants.redDark,
-                          Constants.orangeDark,
-                        ],
-                      )),
-                    ),
-                  ],
+                ],
+                shape: BoxShape.circle,
+              ),
+              child: const CircleAvatar(
+                backgroundColor: Colors.pink,
+                radius: 30,
+                child: FlutterLogo(
+                  size: 40,
                 ),
               ),
-            Spacer(),
+            )
+          else
             IconButton(
-              color: Colors.white,
-              iconSize: 30,
-              onPressed: () {},
-              icon: const Icon(Icons.search),
-            ),
-            Stack(
-              children: [
-                IconButton(
-                  color: Colors.white,
-                  iconSize: 30,
-                  onPressed: () {},
-                  icon: const Icon(Icons.notifications_none_outlined),
-                ),
-                const Positioned(
-                  child: CircleAvatar(
-                      backgroundColor: Colors.pink,
-                      radius: 8,
-                      child: Text(
-                        "3",
+                onPressed: () {
+                  Scaffold.of(context).openDrawer();
+                },
+                iconSize: 30,
+                color: Colors.white,
+                icon: const Icon(Icons.menu)),
+          const SizedBox(
+            width: Constants.kPadding,
+          ),
+          Spacer(),
+          if (ResponsiveLayout.isComputer(context))
+            ...List.generate(
+              _buttonNames.length,
+              (index) => TextButton(
+                onPressed: () {
+                  setState(() {
+                    _currentSelectedButton = index;
+                  });
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(Constants.kPadding * 2),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        _buttonNames[index],
                         style: TextStyle(
-                          fontSize: 10,
-                          color: Colors.white,
+                          color: _currentSelectedButton == index
+                              ? Colors.white
+                              : Colors.white70,
                         ),
-                      )),
+                      ),
+                      Container(
+                        margin: const EdgeInsets.all(Constants.kPadding / 2),
+                        width: 60,
+                        height: 2,
+                        decoration: BoxDecoration(
+                            gradient: _currentSelectedButton == index
+                                ? const LinearGradient(
+                                    colors: [
+                                      Constants.redDark,
+                                      Constants.orangeDark,
+                                    ],
+                                  )
+                                : null),
+                      )
+                    ],
+                  ),
                 ),
-              ],
+              ),
+            )
+          else
+            Padding(
+              padding: const EdgeInsets.all(Constants.kPadding * 2),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    _buttonNames[_currentSelectedButton],
+                    style: const TextStyle(color: Colors.white),
+                  ),
+                  Container(
+                    margin: const EdgeInsets.all(Constants.kPadding / 2),
+                    width: 60,
+                    height: 2,
+                    decoration: const BoxDecoration(
+                        gradient: LinearGradient(
+                      colors: [
+                        Constants.redDark,
+                        Constants.orangeDark,
+                      ],
+                    )),
+                  ),
+                ],
+              ),
             ),
-          ],
-        ));
+          Spacer(),
+          IconButton(
+            color: Colors.white,
+            iconSize: 30,
+            onPressed: () {},
+            icon: const Icon(Icons.search),
+          ),
+          Stack(
+            children: [
+              IconButton(
+                color: Colors.white,
+                iconSize: 30,
+                onPressed: () {},
+                icon: const Icon(Icons.notifications_none_outlined),
+              ),
+              const Positioned(
+                right: 6,
+                top: 6,
+                child: CircleAvatar(
+                    backgroundColor: Colors.pink,
+                    radius: 8,
+                    child: Text(
+                      "3",
+                      style: TextStyle(
+                        fontSize: 10,
+                        color: Colors.white,
+                      ),
+                    )),
+              ),
+            ],
+          ),
+          if (!ResponsiveLayout.isPhone(context))
+            Container(
+              margin: const EdgeInsets.all(Constants.kPadding),
+              height: double.infinity,
+              decoration: const BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black45,
+                    offset: Offset(0, 0),
+                    spreadRadius: 1,
+                    blurRadius: 10,
+                  ),
+                ],
+                shape: BoxShape.circle,
+              ),
+              child: const CircleAvatar(
+                backgroundColor: Constants.orangeDark,
+                radius: 30,
+                child: FlutterLogo(
+                  size: 40,
+                ),
+              ),
+            ),
+        ],
+      ),
+    );
   }
 }
--- lib/panel_left/panel_left_page.dart
+++ lib/panel_left/panel_left_page.dart
@@ -1,3 +1,5 @@
+import 'package:adminpanel/constants.dart';
+import 'package:adminpanel/responsve_layout.dart';
 import 'package:flutter/material.dart';
 
 class PanelLeftPage extends StatefulWidget {
@@ -11,8 +13,22 @@ class _PanelLeftPageState extends State<PanelLeftPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
+      body: Stack(
+        children: [
+          if (ResponsiveLayout.isComputer(context))
+            Container(
+              color: Constants.purpleLight,
+              width: 50,
+              child: Container(
+                decoration: const BoxDecoration(
+                  color: Constants.purpleDark,
+                  borderRadius: BorderRadius.only(
+                    topLeft: Radius.circular(50),
+                  ),
+                ),
+              ),
+            ),
+        ],
       ),
     );
   }
