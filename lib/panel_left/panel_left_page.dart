144b39773b06f13b56ba0f1550687509eb101997
shahbajjamil <shahbajjamil2014@gmail.com>
2021-10-03 23:00:16 +0530

    left page half done

--- lib/panel_left/panel_left_page.dart
+++ lib/panel_left/panel_left_page.dart
@@ -2,6 +2,12 @@ import 'package:adminpanel/constants.dart';
 import 'package:adminpanel/responsve_layout.dart';
 import 'package:flutter/material.dart';
 
+class Todo {
+  String name;
+  bool enable;
+  Todo({required this.name, this.enable = true});
+}
+
 class PanelLeftPage extends StatefulWidget {
   const PanelLeftPage({Key? key}) : super(key: key);
 
@@ -10,6 +16,14 @@ class PanelLeftPage extends StatefulWidget {
 }
 
 class _PanelLeftPageState extends State<PanelLeftPage> {
+  final List<Todo> _toDo = [
+    Todo(name: "Purchase Paper", enable: true),
+    Todo(name: "Refill the inventory of speakers", enable: true),
+    Todo(name: "Hire someone", enable: true),
+    Todo(name: "Marketing Strategy", enable: true),
+    Todo(name: "Selling furniture", enable: true),
+    Todo(name: "Finish the disclousre", enable: true),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -28,6 +42,82 @@ class _PanelLeftPageState extends State<PanelLeftPage> {
                 ),
               ),
             ),
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Constants.kPadding / 2,
+                      right: Constants.kPadding / 2,
+                      top: Constants.kPadding / 2),
+                  child: Card(
+                    color: Constants.purpleLight,
+                    elevation: 2,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Container(
+                      width: double.infinity,
+                      child: const ListTile(
+                        title: Text(
+                          "Products Sold",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        subtitle: Text(
+                          "18% of Products Sold",
+                          style: TextStyle(
+                            color: Colors.white,
+                          ),
+                        ),
+                        trailing: Chip(
+                          label: Text(
+                            "4500",
+                            style: TextStyle(
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                //graph1
+                //graph2
+                Padding(
+                  padding: const EdgeInsets.only(
+                      left: Constants.kPadding / 2,
+                      right: Constants.kPadding / 2,
+                      top: Constants.kPadding / 2),
+                  child: Card(
+                    color: Constants.purpleLight,
+                    elevation: 3,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: Column(
+                      children: List.generate(
+                        _toDo.length,
+                        (index) => CheckboxListTile(
+                          title: Text(
+                            _toDo[index].name,
+                            style: const TextStyle(color: Colors.white),
+                          ),
+                          value: _toDo[index].enable,
+                          onChanged: (newValue) {
+                            setState(() {
+                              _toDo[index].enable = newValue ?? true;
+                            });
+                          },
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          )
         ],
       ),
     );
