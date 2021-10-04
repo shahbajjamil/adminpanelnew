0d22136a2789ad2ab4be9fa4a2174e8afd0983ae
shahbajjamil <shahbajjamil2014@gmail.com>
2021-10-04 12:23:08 +0530

    right panel and bottom app bar done.

--- lib/panel _right/panel_right_page.dart
+++ lib/panel _right/panel_right_page.dart
@@ -1,4 +1,12 @@
+import 'package:adminpanel/constants.dart';
+import 'package:adminpanel/panel%20_right/wiggle_grap.dart';
 import 'package:flutter/material.dart';
+
+class Product {
+  String name;
+  bool enable;
+  Product({required this.name, this.enable = true});
+}
 
 class PanelRightPage extends StatefulWidget {
   const PanelRightPage({Key? key}) : super(key: key);
@@ -8,11 +16,95 @@ class PanelRightPage extends StatefulWidget {
 }
 
 class _PanelRightPageState extends State<PanelRightPage> {
+  final List<Product> _products = [
+    Product(name: "LED Submersible Lights", enable: true),
+    Product(name: "portable Projector", enable: true),
+    Product(name: "Blutooth Speaker", enable: true),
+    Product(name: "Smart Watch", enable: true),
+    Product(name: "Temporary Tattoos", enable: true),
+    Product(name: "Bookends", enable: true),
+    Product(name: "Vegetable Chopper", enable: true),
+    Product(name: "Neck Massager", enable: true),
+    Product(name: "Facial Cleanser", enable: true),
+    Product(name: "Back Cushion", enable: true),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.red,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: Constants.kPadding / 2,
+                  right: Constants.kPadding / 2,
+                  top: Constants.kPadding / 2),
+              child: Card(
+                color: Constants.purpleLight,
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(50),
+                ),
+                child: Container(
+                  width: double.infinity,
+                  child: const ListTile(
+                    title: Text(
+                      "Net Revenue",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    subtitle: Text(
+                      "7% of Sales Avg.",
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                    trailing: Chip(
+                      label: Text(
+                        r"$46,450",
+                        style: TextStyle(
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            LineChartSample1(),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: Constants.kPadding / 2,
+                  right: Constants.kPadding / 2,
+                  top: Constants.kPadding / 2),
+              child: Card(
+                color: Constants.purpleLight,
+                elevation: 3,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(30),
+                ),
+                child: Column(
+                  children: List.generate(
+                      _products.length,
+                      (index) => SwitchListTile.adaptive(
+                            activeColor: Constants.greenLight,
+                            value: _products[index].enable,
+                            onChanged: (newValue) {
+                              setState(() {
+                                _products[index].enable = newValue;
+                              });
+                            },
+                            title: Text(
+                              _products[index].name,
+                              style: const TextStyle(color: Colors.white),
+                            ),
+                          )),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- lib/widget_tree.dart
+++ lib/widget_tree.dart
@@ -1,9 +1,11 @@
 import 'package:adminpanel/Panel_left/panel_left_page.dart';
 import 'package:adminpanel/app_bar/app_bar_widget.dart';
+import 'package:adminpanel/constants.dart';
 import 'package:adminpanel/drawer/drawer_page.dart';
 import 'package:adminpanel/panel%20_right/panel_right_page.dart';
 import 'package:adminpanel/panel_center/panel_center_page.dart';
 import 'package:adminpanel/responsve_layout.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 
 class WidgetTree extends StatefulWidget {
@@ -14,6 +16,13 @@ class WidgetTree extends StatefulWidget {
 }
 
 class _WidgetTreeState extends State<WidgetTree> {
+  int currentINdex = 1;
+
+  List<Widget> _icons = [
+    Icon(Icons.add, size: 30),
+    Icon(Icons.list, size: 30),
+    Icon(Icons.compare_arrows, size: 30),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,12 +30,16 @@ class _WidgetTreeState extends State<WidgetTree> {
         child: (ResponsiveLayout.isTinyLimit(context) ||
                 ResponsiveLayout.isTinyHeightLimit(context))
             ? Container()
-            :const AppBarWidget(),
+            : const AppBarWidget(),
         preferredSize: const Size(double.infinity, 100),
       ),
       body: ResponsiveLayout(
         tiny: Container(),
-        phone: const PanelCenterPage(),
+        phone: currentINdex == 0
+            ? const PanelLeftPage()
+            : currentINdex == 1
+                ? const PanelCenterPage()
+                : const PanelRightPage(),
         tablet: Row(
           children: const [
             Expanded(child: PanelLeftPage()),
@@ -50,6 +63,18 @@ class _WidgetTreeState extends State<WidgetTree> {
         ),
       ),
       drawer: const Drawerpage(),
+      bottomNavigationBar: ResponsiveLayout.isPhone(context)
+          ? CurvedNavigationBar(
+              index: currentINdex,
+              backgroundColor: Constants.purpleDark,
+              items: _icons,
+              onTap: (index) {
+                setState(() {
+                  currentINdex = index;
+                });
+              },
+            )
+          : SizedBox(),
     );
   }
 }
